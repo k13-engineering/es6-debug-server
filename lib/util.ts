06c853a92694f3da3fdf5170ed84f6e5f6b3bd86
Simon Kadisch <simon.kadisch@k13-engineering.at>
2024-04-29 22:37:48 +0200

    Make Typescript happy

--- lib/util.ts
+++ lib/util.ts
@@ -6,7 +6,7 @@ interface IGenericSuccessResponse {
     error: undefined;
 };
 
-type TMaybeError<T, U = Error> = (T & IGenericSuccessResponse) | (IGenericErrorResponse<U> & {
+type TMaybeError<T, U = globalThis.Error> = (T & IGenericSuccessResponse) | (IGenericErrorResponse<U> & {
     [K in keyof T]?: undefined;
 });
 
